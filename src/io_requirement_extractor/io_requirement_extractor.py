730d003232f42fe9539c77c4543f659229f6d1d2
Hariharan Devarajan <mani.hariharan@gmail.com>
2020-08-18 18:07:15 -0500

    added samples and code flow for extractor.

--- src/io_requirement_extractor/io_requirement_extractor.py
+++ src/io_requirement_extractor/io_requirement_extractor.py
@@ -14,10 +14,16 @@ class IORequirementExtractor:
         self._initialize()
 
         #set the value from parser into input.
+
+        # TODO: neeraj set the input from darshan parser
+        # load sample/darshan.json into input
         input = None
+        # call to database to check if key:input exists if true skip mapping
+        # load sample/io_req_output.json into output
         output = JSONClient().load("io_requirement.json")
         mapper = MapperManager()
         mapper.run(input, output)
+        # call to database to store it key:input, val:output
         self._finalize()
         return output
 
