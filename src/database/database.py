a723d4b0a26be1f941095f8515502edd8919ab04
Jie Ye <jye20@hawk.iit.edu>
2020-08-18 16:04:08 -0500

    Modify database.py a singleton class

--- src/database/database.py
+++ src/database/database.py
@@ -3,9 +3,33 @@ from src.common.enumerations import KVStoreType
 
 import typing
 
-class DataBase:
+class DataBase(object):
+    _instance = None
+
+    @staticmethod
+    def get_instance():
+        """Static access method"""
+        if DataBase._instance is None:
+            DataBase()
+        return DataBase._instance
+
     def __init__(self) -> None:
+        super().__init__()
+        """Virtually private constructor"""
+        if DataBase._instance is not None:
+            raise Exception("The DataBase class is a singleton!")
+        else:
+            DataBase._instance = self
+        self.kvstore = None
+
+    def _initialize(self) -> None:
         self.kvstore = KVStoreFactory.get_kv_store(KVStoreType.REDIS)
+        print("init kvstore object: ", self.kvstore)
+
+    def _finalize(self) -> None:
+        del self.kvstore
+        self.kvstore = None
+        print(self.kvstore)
 
     def put(self, key, value):
         self.kvstore.put(key, value)
