ade1b1d9fc122d3eade9911a7c0ec60ea18ee1d2
Neeraj Rajes <neeraj@room429>
2020-08-18 22:16:28 -0500

    darshan extraction done

--- src/io_requirement_extractor/trace_parser/darshan_trace_parser.py
+++ src/io_requirement_extractor/trace_parser/darshan_trace_parser.py
@@ -1,9 +1,11 @@
 import darshan
-from io_requirement_extractor.trace_parser import TraceParser
+from src.io_requirement_extractor.trace_parser import TraceParser
+from typing import List, Dict, Tuple
 
-class DarshanTraceParser():
-    extracted_variables = {}
-    sum_counters = [
+
+class DarshanTraceParser(TraceParser):
+    _extracted_variables = {}
+    _sum_counters = [
         'total_reads',
         'total_writes'
         'total_bytes_read',
@@ -15,7 +17,18 @@ class DarshanTraceParser():
     def __init__(self) -> None:
         pass
 
-    def _getTotalModuleStats(self, module_: str):
+    def _getMaxByteOp(self) -> List[int]:
+        prefix = ['POSIX', 'STDIO']
+        nome = '_MAX_BYTE_'
+        read_ = []
+        write_ = []
+        for i in prefix:
+            for j in self.dar_dict[i]:
+                read_.append(j['counters'][f'{i}{nome}READ'])
+                write_.append(j['counters'][f'{i}{nome}WRITTEN'])
+        return [max(read_), max(write_)]
+
+    def _getTotalModuleStats(self, module_: str) -> List[float]:
         module = 'MPIIO' if module_ == 'MPI-IO' else module_
         rw_prefixs = ['MPIIO_COLL', 'MPIIO_INDEP', 'MPIIO_SPLIT', 'MPIIO_NB'] \
             if module == 'MPIIO' else [module]
@@ -51,7 +64,28 @@ class DarshanTraceParser():
             sum(all_write_time_est)
         ]
 
-    def _getConsecs(self):
+    def _getMaxOpTimeSize(self) -> List[Tuple[float, float]]:
+        modules = ['POSIX', 'MPI-IO', 'H5D']
+        read_time = 0
+        write_time = 0
+        max_read = 0
+        max_write = 0
+        for i in modules:
+            for j in self.dar_dict[i]:
+                mod = 'MPIIO' if i == 'MPI-IO' else i
+                if j['fcounters'][f'{mod}_F_MAX_READ_TIME'] > read_time:
+                    read_time = j['fcounters'][f'{mod}_F_MAX_READ_TIME']
+                    max_read = j['counters'][f'{mod}_MAX_READ_TIME_SIZE']
+
+                if j['fcounters'][f'{mod}_F_MAX_WRITE_TIME'] > read_time:
+                    write_time = j['fcounters'][f'{mod}_F_MAX_WRITE_TIME']
+                    max_write = j['counters'][f'{mod}_MAX_WRITE_TIME_SIZE']
+        return [
+            (read_time, max_read),
+            (write_time, max_write)
+        ]
+
+    def _getConsecs(self) -> List[int]:
         consec_read = []
         consec_write = []
         for i in self.dar_dict['POSIX']:
@@ -59,7 +93,7 @@ class DarshanTraceParser():
             consec_write.append(i['counters']['POSIX_CONSEC_WRITES'])
         return [sum(consec_read), sum(consec_write)]
 
-    def parse(self, file_: str) -> None:
+    def parse(self, file_: str) -> Dict[str, float]:
         self.report = darshan.DarshanReport(file_, read_all=True)
         self.dar_dict = self.report.records_as_dict()
         modules_list = list(self.dar_dict.keys())
@@ -69,14 +103,24 @@ class DarshanTraceParser():
         for i in modules_list:
             total.append(self._getTotalModuleStats(i))
 
-        for i, j in zip(self.sum_counters, map(sum, zip(*total))):
-            self.extracted_variables[i] = j
+        for i, j in zip(self._sum_counters, map(sum, zip(*total))):
+            self._extracted_variables[i] = j
 
-        self.extracted_variables['total_consec_reads'], \
-            self.extracted_variables['total_consec_writes'] \
+        self._extracted_variables['total_consec_reads'], \
+            self._extracted_variables['total_consec_writes'] \
             = self._getConsecs()
 
-        return self.extracted_variables
+        self._extracted_variables['max_byte_read'], \
+            self._extracted_variables['max_byte_written'] \
+            = self._getMaxByteOp()
+
+        (self._extracted_variables['max_read_time'],
+         self._extracted_variables['max_read_time_size']), \
+            (self._extracted_variables['max_write_time'],
+             self._extracted_variables['max_write_time_size']) \
+            = self._getMaxOpTimeSize()
+
+        return self._extracted_variables
 
     def _finalize(self) -> None:
         pass
--- src/io_requirement_extractor/trace_parser/trace_parser_factory.py
+++ src/io_requirement_extractor/trace_parser/trace_parser_factory.py
@@ -3,10 +3,12 @@ from common.enumerations import *
 from common.error_codes import *
 from io_requirement_extractor.trace_parser.trace_parser import *
 from io_requirement_extractor.trace_parser.darshan_trace_parser import *
+from type import Type
+
 
 class TraceParserFactory:
 
-    DARSHAN='darshan'
+    DARSHAN = 'darshan'
 
     _parse_classes = {
         DARSHAN: DarshanTraceParser,
@@ -15,7 +17,7 @@ class TraceParserFactory:
     def __init__(self):
         pass
 
-    def get(self, parser_id: int):
+    def get(self, parser_id: int) -> Type[TraceParser]:
         try:
             return TraceParserFactory._parse_classes[parser_id]()
         except:
