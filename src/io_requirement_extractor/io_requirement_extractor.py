9aa795f2f4ad1b97fd0e1f40d34aec0330ba95b2
Neeraj Rajesh <10393055+nrj5k@users.noreply.github.com>
2020-08-19 15:14:25 -0500

    Update io_requirement_extractor.py

--- src/io_requirement_extractor/io_requirement_extractor.py
+++ src/io_requirement_extractor/io_requirement_extractor.py
@@ -23,7 +23,6 @@ class IORequirementExtractor:
         output = darshan_parser.parse(input_)
         # call to database to check if key:input exists if true skip mapping
         db = DataBase.get_instance()
-<<<<<<< HEAD
         exist = DataBase.query(input_)
         if exist:
             output = DataBase.get(input_)
@@ -35,12 +34,6 @@ class IORequirementExtractor:
             # call to database to store it key:input, val:output
             db.put(input, output)
         self._finalize()
-=======
-        try:
-            return db.get(input)
-        except:
-            pass
->>>>>>> 66bdc06de58fb8479990052d6c6cd789a820a87e
 
         # load sample/io_req_output.json into output
         output = JSONClient().load(conf.io_req_out_path)
