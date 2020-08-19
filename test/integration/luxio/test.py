7706fe2ea98f4ce6d2b37745e30754f94a1fdab8
lukemartinlogan <lukemartinlogan@gmail.com>
2020-08-19 16:48:40 -0500

    Fix stor_req_conf_output.json

--- test/integration/luxio/test.py
+++ test/integration/luxio/test.py
@@ -2,10 +2,11 @@ import unittest
 from common.error_codes import *
 from common.enumerations import *
 from common.configuration_manager import *
+from external_clients.json_client import *
 from luxio import *
 
 class TestLuxio(unittest.TestCase):
-    def test_luxio(self):
+    def test_luxio_sample(self):
         conf = ConfigurationManager.get_instance()
         conf.job_spec="sample/job_info.json"
         conf.darshan_trace_path="sample/sample.darshan"
@@ -18,8 +19,54 @@ class TestLuxio(unittest.TestCase):
 
         tool = LUXIO()
         config = tool.run()
+        JSONClient().dumps(config)
+
+"""
+    def test_luxio_vpic(self):
+        conf = ConfigurationManager.get_instance()
+        conf.job_spec="sample/job_info.json"
+        conf.darshan_trace_path="sample/vpic.darshan"
+        conf.io_req_out_path="sample/io_req_output.json"
+        conf.storage_req_out_path="sample/stor_req_output.json"
+        conf.storage_req_config_out_path="sample/stor_req_conf_output.json"
+        conf.db_type = KVStoreType.REDIS
+        conf.db_addr="127.0.0.1"
+        conf.db_port="6379"
+
+        tool = LUXIO()
+        config = tool.run()
         print(config)
 
+    def test_luxio_hacc_io_read(self):
+        conf = ConfigurationManager.get_instance()
+        conf.job_spec="sample/job_info.json"
+        conf.darshan_trace_path="sample/hacc_io_read.darshan"
+        conf.io_req_out_path="sample/io_req_output.json"
+        conf.storage_req_out_path="sample/stor_req_output.json"
+        conf.storage_req_config_out_path="sample/stor_req_conf_output.json"
+        conf.db_type = KVStoreType.REDIS
+        conf.db_addr="127.0.0.1"
+        conf.db_port="6379"
+
+        tool = LUXIO()
+        config = tool.run()
+        print(config)
+
+    def test_luxio_hacc_io_write(self):
+        conf = ConfigurationManager.get_instance()
+        conf.job_spec="sample/job_info.json"
+        conf.darshan_trace_path="sample/hacc_io_write.darshan"
+        conf.io_req_out_path="sample/io_req_output.json"
+        conf.storage_req_out_path="sample/stor_req_output.json"
+        conf.storage_req_config_out_path="sample/stor_req_conf_output.json"
+        conf.db_type = KVStoreType.REDIS
+        conf.db_addr="127.0.0.1"
+        conf.db_port="6379"
+
+        tool = LUXIO()
+        config = tool.run()
+        print(config)
+"""
 
 if __name__ == "__main__":
     unittest.main()
