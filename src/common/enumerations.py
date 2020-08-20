99eddd85dd307a17f902efdd61cb1ffa5e0c80ac
Jie Ye <jye20@hawk.iit.edu>
2020-08-20 16:07:58 -0500

    Add comments to all the core code

--- src/common/enumerations.py
+++ src/common/enumerations.py
@@ -1,21 +1,26 @@
 from enum import Enum
 
-"""Key-value store types supported in Luxio"""
+"""
+Key-value store types supported in Luxio
+"""
 class KVStoreType(Enum):
     REDIS = "redis"
 
     def __str__(self):
         return self.value
 
-"""Trace Parser Types supported in Luxio"""
+"""
+Trace Parser Types supported in Luxio
+"""
 class TraceParserType(Enum):
     DARSHAN = 'darshan'
 
     def __str__(self) -> str:
         return self.value
 
-
-"""Serialization Libraries supported"""
+"""
+Serialization Libraries supported in Luxio
+"""
 class SerializerType(Enum):
     PICKLE = "pickle"
     MSGPACK = "message_pack"
@@ -23,11 +28,12 @@ class SerializerType(Enum):
     def __str__(self):
         return self.value
 
-"""Storage Configuarator Libraries supported in Luxio"""
+"""
+Storage Configuarator Libraries supported in Luxio
+"""
 class StorageConfiguratorType(Enum):
     ORANGEFS = "orangefs"
 
     def __str__(self):
         return self.value
-
 
--- src/common/error_codes.py
+++ src/common/error_codes.py
@@ -8,6 +8,11 @@ class Error(BaseException):
             self._error_message = str(child_error) + "\n{}".format(error_code["msg"])
 
     def format(self, *error_code_args):
+        """
+        Formatted the error message
+        :param error_code_args:
+        :return: Error
+        """
         self._error_message = self._error_message.format(error_code_args)
         return self
 
@@ -18,25 +23,27 @@ class Error(BaseException):
         return self._error_message
 
 class ErrorCode:
+    """
+    A class shows all the error code in Luxio
+    """
     NONE = {"id": 0, "msg": "SUCCESSFUL"}
 
-    #General
+    #General error code
     NOT_IMPLEMENTED = {"id": 1, "msg": "{} is not implemented"}
     TOO_MANY_INSTANCES = {"id": 2, "msg": "{} is a singleton class, but has been initialized more than once"}
     INVALID_ENUM = {"id": 3, "msg": "{}: {} is not a valid enum"}
 
-    #Serializers
+    #Serializers module error code
     INVALID_SERIAL_DICT = {"id": 1000, "msg": "{}: Could not serialize dict"}
     INVALID_DESERIAL_STR = {"id": 1001, "msg": "{}: Could not deserialize string"}
 
-    #Mapper
+    #MapperManager error code
     MAPPER_EXEC_ERROR = {"id": 2000, "msg": "MapperManager: Generic execution error in {}: {}"}
 
-
-    #Trace Parser
+    #Trace Parser module error code
     PARSER_ID = {"id": 4000, "msg": "ParserFactory: Invalid Parser ID: {}"}
 
-    # DataBase
+    # DataBase module error code
     INVALID_KV_STORE_TYPE = {"id": 3000, "msg": "KVStoreFactory: Invalid key-value store type: {}"}
     REDISDB_STORE_ERROR = {"id": 3001, "msg": "RedisDB: Failed to store data into Redis"}
     REDISDB_GET_ERROR = {"id": 3002, "msg": "RedisDB: Failed to get data from Redis"}
