662dff0ec9225a89835edc94b6b0138d7218e52b
lukemartinlogan <lukemartinlogan@gmail.com>
2020-08-19 21:14:36 -0500

    Add setup.py. Update readme to show how to use.

--- setup.py
+++ setup.py
@@ -1,6 +1,23 @@
-from distutils.core import setup
+import setuptools
 
-setup(
-    name="luxio"
-    packages=[]
+setuptools.setup(
+    name="luxio",
+    packages=setuptools.find_packages(),
+    version="0.0.1",
+    author="Keith Bateman",
+    author_email="kbateman@hawk.iit.edu",
+    description="A tool for automatically configuring storage for distributed applications",
+    url="https://github.com/scs-lab/luxio",
+    classifiers = [
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Development Status :: 0 - Pre-Alpha",
+        "Environment :: Other Environment",
+        "Intended Audience :: Developers",
+        "License :: None",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Distributed Job Scheduling",
+    ],
+    long_description=""
 )
