8aec6f06be362d3aaaf620b5adf420d0de208e21
lukemartinlogan <lukemartinlogan@gmail.com>
2021-01-06 11:32:12 -0600

    OOP works for ensemble with a single XGBoost

--- statistics/src/curve_wrapper.py
+++ statistics/src/curve_wrapper.py
@@ -1,10 +1,11 @@
-from generic_model import GenericModel
+from src.generic_model import GenericModel
 
 class CurveWrapper(GenericModel):
     def __init__(self, model):
-        self.model_ = model
+        self.model = model
         self.feature_importances_ = None
+        self.fitness_ = 0
 
-    def feature_importances(self):
-        abscoeff = np.absolute(model.coef_[0])
+    def calculate_importances_(self):
+        abscoeff = np.absolute(self.model.coef_[0])
         self.feature_importances_ = abscoeff / sum(abscoeff)
