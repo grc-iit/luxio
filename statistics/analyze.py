775cd081e85cc4a7346592ffbd1af0c313fdd004
lukemartinlogan <lukemartinlogan@gmail.com>
2021-01-13 17:25:33 -0600

    Add args to analyzer

--- statistics/analyze.py
+++ statistics/analyze.py
@@ -1,10 +1,9 @@
 
 """
 This file is used for three purposes:
-    1) Identify important features relating to I/O and to identify classes of I/O
-behavior.
-
-For feature reduction, the output of this file will be a csv containing feature names and importances.
+    1) To preprocess a trace dataset
+    2) Identify important features relating to I/O performance
+    3) To identify classes of I/O behavior based on those features
 """
 
 import sys,os
@@ -13,33 +12,76 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from clever.dataset import *
-from clever.models import *
+from clever.models.regression import *
 from clever.transformers import *
 from clever.feature_selection import *
+from clever.models.clustering import *
 
 import pprint, warnings
+import argparse
 
-def usage():
-    print("Usage: ")
+pp = pprint.PrettyPrinter(depth=6)
+
+class ArgumentParser:
+    def __init__(self):
+        self.parser = argparse.ArgumentParser()
+        self.parser.add_argument("-tool", default=None, help="What your use case is: partition, fmodel, fmodel_stats, bmodel, bmodel_stats")
+        self.parser.add_argument("-trace", default="datasets/preprocessed_dataset.csv", help="The path to the trace data")
+        self.parser.add_argument("-m", default="datasets/model/model.pkl", help="The file to save the feature reduction or app behavior model to")
+        self.parser.add_argument("-f", default="features/features.csv", help="The CSV containing features")
+        self.parser.add_argument("-v", default="features/performance.csv", help="The CSV containing variables")
+        self.parser.add_argument("-i", default="features/importances.csv", help="The CSV containing feature importances")
+
+    def parse(self):
+        args = self.parser.parse_args()
+        self.tool = args.tool
+        self.trace_path = args.trace
+        self.model_path = args.m
+        self.features_path = args.f
+        self.vars_path = args.v
+        self.importances_path = args.i
+        return self
 
 ##############MAIN##################
 if __name__ == "__main__":
-    if case == 1:
+    args = ArgumentParser().parse()
+
+    if args.tool == "partition":
         lt = LogTransformer(base=10, add = 1)
-        df = pd.read_csv("datasets/preprocessed_dataset.csv")
-        #df = df.clever.transform(lt, True)
-        #partitions = df.clever.kmeans(features="TOTAL_IO_TIME", k=10, cluster_col="partition").agglomerate(dist_thresh=.5)
+        df = pd.read_csv(args.trace_path)
+        var = pd.read_csv(args.vars_path)
+        df.loc[:,var] = df.clever.transform(lt, True)
+        partitions = df.clever.kmeans(features=var, k=10, cluster_col="partition").agglomerate(dist_thresh=.5)
         #df_wrap = df.clever.inverse(lt)
-        partitions = df.clever.exp_partition("TOTAL_IO_TIME", base=10, exp=2.7, min_n=500, cluster_col="partition")
-        pp.pprint(partitions.analyze()["TOTAL_IO_TIME"])
+        #partitions = df.clever.exp_partition(var, base=10, exp=2.7, min_n=500, cluster_col="partition")
+        pp.pprint(partitions.analyze()[var])
         #pd.DataFrame(analysis["TOTAL_IO_TIME"]).transpose().round(decimals=3).to_csv("datasets/partition-stats.csv")
 
-    if case == 2:
-        FEATURES = pd.DataFrame().clever.load_features("features/features.csv")
-        PERFORMANCE = pd.DataFrame().clever.load_features("features/performance.csv")
-        fs = FeatureSelector(FEATURES, PERFORMANCE, "datasets/preprocessed_dataset.csv", model_dir="datasets/model")
+    if args.tool == "fmodel":
+        FEATURES = pd.DataFrame().clever.load_features(args.features_path)
+        PERFORMANCE = pd.DataFrame().clever.load_features(args.vars_path)
+        fs = FeatureSelector(FEATURES, PERFORMANCE, args.trace_path, model_path=args.model_path)
         fs.create_model()
-
-    if case == 3:
-        fs = FeatureSelector.load("datasets/model/model.pkl")
         fs.analyze_model()
+        fs.save_importances(args.importances_path)
+
+    if args.tool == "fmodel_stats":
+        fs = FeatureSelector.load(args.model_path)
+        fs.analyze_model()
+        fs.save_importances(args.importances_path)
+
+    if args.tool == "bmodel":
+        df = pd.read_csv(args.trace_path)
+        importances = FeatureSelector.load_importances(args.importances_path)
+        wic = WeightedImportanceClassifier(importances)
+        wic.fit(df)
+        wic.save(args.model_path)
+
+    if args.tool == "bmodel_stats":
+        FEATURES = []
+        PERFORMANCE = []
+        if args.features_path != None:
+            FEATURES = pd.DataFrame().clever.load_features(args.features_path)
+        elif args.vars_path != None:
+            PERFROMANCE = pd.DataFrame().clever.load_features(args.vars_path)
+        wic.model.analyze(FEATURES + PERFROMANCE)
