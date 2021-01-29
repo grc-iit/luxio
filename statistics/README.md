b8cab5e11ed7f2a414c10745eb4e7435a19678d2
lukemartinlogan <lukemartinlogan@gmail.com>
2021-01-29 00:16:40 -0600

    Trace parser

--- statistics/README.md
+++ statistics/README.md
@@ -7,27 +7,19 @@ classify applications that exhibit these features into categories of performance
 
 ## Requirements
 
-* matplotlib  
-* numpy  
-* pandas  
-* sklearn  
-* progressbar2  
-* pydot  
-
-## Structure
-
-* preprocess.py: Creates derivative columns and removes invalid entries from the combined Mira/Theta datasets  
-* analyze.py: Analyzes the preprocessed dataset in various ways  
-* datasets: A directory that contains the datasets used for the analysis  
-* datasets/dataset.csv: The combined trace information from Mira and Theta  
-* datasets/preprocessed_dataset.csv: The dataset that contains derivative columns and no invalid entries  
-* datasets/preprocessed_schema.csv: The set of columns that the preprocessed_dataset.csv contains  
-* datasets/features.csv: The set of candidate features to use to describe performance  
-* datasets/performance.csv: The set of performance variables we are trying to model  
-* datasets/traces: Contains the unmodified trace data from Mira and Theta  
+* clever 0.0.0
 
 ## Usage
 
-preprocess.py has no inputs. It reads dataset/dataset.csv, removes invalid entries, and adds derivative columns.  
+```{bash}
+python3 analyze.py -t [tool] -c [conf]  
+```
 
-analyze.py has no inputs. You can look at the different cases provided at the bottom of the file.  
+[tool]:  
+* preprocess: preprocess data from ANL  
+* fmodel: Feature reduction model  
+* fmodel_stats: Statistics about the reduced model  
+* bmodel: Behavior classifier using the stats  
+* bmodel_stats: Statistics about the behavior classification  
+
+An example configuration file is under /conf  
--- statistics/analyze.py
+++ statistics/analyze.py
@@ -10,13 +10,13 @@ import sys,os
 import pandas as pd
 import numpy as np
 
+from parse import *
 from clever.dataset import *
 from clever.models.regression import *
 from clever.transformers import *
 from clever.feature_selection import *
 from clever.models.clustering import *
 from clever.metrics import *
-from sklearn.preprocessing import MinMaxScaler
 
 import pprint, warnings
 import argparse, configparser
@@ -26,8 +26,9 @@ pp = pprint.PrettyPrinter(depth=6)
 class ArgumentParser:
     def __init__(self):
         self.parser = argparse.ArgumentParser()
-        self.parser.add_argument("-t", default=None, help="What your use case is: fmodel, fmodel_stats, bmodel, bmodel_stats")
+        self.parser.add_argument("-t", default=None, help="What your use case is: preprocess, fmodel, fmodel_stats, bmodel, bmodel_stats")
         self.parser.add_argument("-c", default="conf/conf.ini", help="The properties file containing model paths and config parameters. Default: conf/conf.ini")
+        self.parser.add_argument("-m", default="Argonne", help="Which data to be preprocessed: Argonne, Tiahu")
 
     def parse(self):
         args = self.parser.parse_args()
@@ -36,57 +37,103 @@ class ArgumentParser:
         self.conf.read(args.c)
         return self
 
+#Feature selection module
+def feature_selector(params):
+    FEATURES = pd.DataFrame().clever.load_features(params["features"])
+    #IMM_FEATURES = pd.DataFrame().clever.load_features(params["imm_features"])
+    PERFORMANCE = pd.DataFrame().clever.load_features(params["vars"])
+    #Create the training and testing datasets
+    df = pd.read_csv(params["trace"])
+    train_df,hyper_df,test_df = df.clever.random_sample(.5, .2)
+    train_df.to_pickle(params["train"])
+    hyper_df.to_pickle(params["hyper"])
+    test_df.to_pickle(params["test"])
+    #Divide the datasets into x and y
+    train_x,train_y = train_df.clever.split(FEATURES,PERFORMANCE)
+    hyper_x,hyper_y = hyper_df.clever.split(FEATURES,PERFORMANCE)
+    test_x,test_y = test_df.clever.split(FEATURES,PERFORMANCE)
+    #Partition by performance
+    partitioner = KSegments(k=5).fit(np.array(LogTransformer(base=10,add=1).transform(train_y)).flatten())
+    #Select features
+    fs = FeatureSelector(
+        FEATURES,
+        PERFORMANCE,
+        XGBRegressor(
+            transform_y=LogTransformer(base=10,add=1),
+            fitness_metric=PartitionedMetric(partitioner, score=RelativeAccuracyMetric(scale=1, add=1)),
+            error_metric=PartitionedMetric(partitioner, score=RMLSEMetric(add=1)))
+    )
+    fs.select(
+        train_x, train_y, hyper_x, hyper_y,
+        #search_space=fs.model.get_search_space(),
+        max_iter=12,
+        max_tunes=0,
+        max_tune_iter=0)
+    #Save and analyze
+    fs.save(params["selector"])
+    fs.model.save(params["regressor"])
+    fs.save_importances(params["importances"])
+    pp.pprint(fs.analyze(test_x,test_y))
+
+#Feature selection statistics module
+def feature_selector_stats(params):
+    FEATURES = pd.DataFrame().clever.load_features(params["features"])
+    PERFORMANCE = pd.DataFrame().clever.load_features(params["vars"])
+    #Create the training and testing datasets
+    train_df = pd.read_pickle(params["train"])
+    hyper_df = pd.read_pickle(params["hyper"])
+    test_df = pd.read_pickle(params["test"])
+    #Divide the datasets into x and y
+    train_x,train_y = train_df.clever.split(FEATURES,PERFORMANCE)
+    hyper_x,hyper_y = train_df.clever.split(FEATURES,PERFORMANCE)
+    test_x,test_y = train_df.clever.split(FEATURES,PERFORMANCE)
+    #Load the feature selector
+    fs = FeatureSelector.load(params["selector"])
+    pp.pprint(fs.analyze(test_x, test_y))
+    fs.save_importances(params["importances"])
+
+#Behavior Classifaction module
+def behavior_classifier(params):
+    params = args.conf["APP_BEHAVIOR_MODEL"]
+    df = pd.read_csv(params["trace"])
+    feature_importances = FeatureSelector.load_importances(params["importances"])
+    vars = pd.DataFrame().clever.load_features(params["vars"])
+    #imm_features = pd.DataFrame().clever.load_features(params["imm_features"])
+    reg = FeatureSelector.load(params["regressor"])
+    bc = BehaviorClassifier(feature_importances, vars, reg)
+    bc.fit(df)
+    bc.save(params["classifier"])
+
+#Behavior Classification statistics module
+def behavior_classifier_stats(params):
+    params = args.conf["APP_BEHAVIOR_MODEL"]
+    bc = BehaviorClassifier.load(params["classifier"])
+    analysis = bc.analyze()
+    pp.pprint(analysis)
+
 ##############MAIN##################
 if __name__ == "__main__":
     args = ArgumentParser().parse()
 
     if args.tool == "preprocess":
-        a = 0
+        params = args.conf["PREPROCESS"]
+        parser = ArgonneParser(params["theta_csv"], params["mira_csv"], params["mira_mapping"])
+        parser.standardize()
+        parser.clean()
+        parser.to_csv(params["argonne_csv"])
 
     if args.tool == "fmodel":
         params = args.conf["APP_BEHAVIOR_MODEL"]
-        FEATURES = pd.DataFrame().clever.load_features(params["features"])
-        PERFORMANCE = pd.DataFrame().clever.load_features(params["vars"])
-        #Create the training and testing datasets
-        df = pd.read_csv(params["trace"])
-        train_df,hyper_df,test_df = df.clever.random_sample(.5, .2)
-        train_x,train_y = train_df.clever.split(FEATURES,PERFORMANCE)
-        hyper_x,hyper_y = train_df.clever.split(FEATURES,PERFORMANCE)
-        #Select features
-        fs = FeatureSelector(
-            FEATURES,
-            PERFORMANCE,
-            PartitionedEnsembleRegressor(
-                transform_y=LogTransformer(base=10,add=1),
-                fitness_metric=RelativeAccuracyMetric(add=1),
-                error_metric=RMLSEMetric(add=1))
-        )
-        fs.select(train_x, train_y, hyper_x, hyper_y, fs.model.tune_config(train_x, train_y, hyper_x, hyper_y), max_tune_iter=0)
-        #Save and analyze
-        fs.save(params["selector"])
-        fs.model.save(params["regressor"])
-        fs.save_importances(params["importances"])
-        fs.analyze()
+        feature_selector(params)
 
     if args.tool == "fmodel_stats":
         params = args.conf["APP_BEHAVIOR_MODEL"]
-        fs = FeatureSelector.load(params["selector"])
-        fs.analyze()
-        fs.save_importances(params["importances"])
+        feature_selector_stats(params)
 
     if args.tool == "bmodel":
         params = args.conf["APP_BEHAVIOR_MODEL"]
-        df = pd.read_csv(params["trace"])
-        importances = FeatureSelector.load_importances(params["importances"])
-        vars = pd.DataFrame().clever.load_features(params["vars"])
-        reg = FeatureSelector.load(params["regressor"])
-        wic = BehaviorClassifier(importances, vars, reg)
-        wic.fit(df)
-        wic.save(params["classifier"])
+        behavior_classifier(params)
 
     if args.tool == "bmodel_stats":
         params = args.conf["APP_BEHAVIOR_MODEL"]
-        wic = BehaviorClassifier.load(params["classifier"])
-        analysis = wic.log_clusters.analyze(method='stat-clust', metric='mean')
-        analysis_df = pd.DataFrame(analysis).clever.transform(MinMaxScaler(), fit=True, features=wic.features + wic.vars)
-        analysis_df.transpose().to_csv(params["class_metrics"])
+        behavior_classifier_stats(params)
