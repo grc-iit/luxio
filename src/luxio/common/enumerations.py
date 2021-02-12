b1645171128feedd949bcc1d34d7e3d61d25c473
Neeraj Rajesh <neeraj@room429>
2021-02-12 14:26:20 -0600

    added resolver base template + factory

--- src/luxio/common/enumerations.py
+++ src/luxio/common/enumerations.py
@@ -1,40 +1,50 @@
 from enum import Enum
 
-"""
-Key-value store types supported in Luxio
-"""
+
 class KVStoreType(Enum):
+    """
+    Key-value store types supported in Luxio
+    """
     REDIS = "redis"
 
     def __str__(self):
         return self.value
 
-"""
-Trace Parser Types supported in Luxio
-"""
+
 class TraceParserType(Enum):
+    """
+    Trace Parser Types supported in Luxio
+    """
     DARSHAN = 'darshan'
     ARGONNE = 'argonne'
 
     def __str__(self) -> str:
         return self.value
 
-"""
-Serialization Libraries supported in Luxio
-"""
+
 class SerializerType(Enum):
+    """
+    Serialization Libraries supported in Luxio
+    """
     PICKLE = "pickle"
     MSGPACK = "message_pack"
 
     def __str__(self):
         return self.value
 
-"""
-Storage Configuarator Libraries supported in Luxio
-"""
+
 class StorageConfiguratorType(Enum):
+    """
+    Storage Configuarator Libraries supported in Luxio
+    """
     ORANGEFS = "orangefs"
 
     def __str__(self):
         return self.value
 
+
+class ResolverType(Enum):
+    """
+    Resolvers supported in Luxio
+    """
+    LUXIO = 'luxio'
