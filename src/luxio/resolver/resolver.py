3ec7ba74e2c6eb2b566a42f66d67b86cb2edfc3d
Neeraj Rajesh <neeraj@room429>
2021-02-25 10:29:24 -0600

    move resolver

--- src/luxio/resolver/resolver.py
+++ src/luxio/resolver/resolver.py
@@ -2,9 +2,6 @@
 import asyncio
 from abc import ABC, abstractmethod
 from luxio.external_clients.json_client import JSONClient
-from luxio.external_clients.kv_store.kv_store_factory import KVStoreType
-from luxio.common.enumerations import KVStoreType
-from configparser import ConfigParser
 
 
 class Resolver(ABC):
@@ -14,14 +11,6 @@ class Resolver(ABC):
 
     def __init__(self) -> None:
         self.loop = asyncio.get_event_loop()
-        self.db = KVStoreType(KVStoreType.REDIS)
-        conf = ConfigParser()
-        conf.read('storage.conf')
-        self.counts = {}
-        for i in conf.sections():
-            val = conf.get(i, 'counts')
-            self.counts[i] = val
-            self.db._put_impl(i, val)
 
     def _initialize(self) -> None:
         pass
@@ -42,27 +31,13 @@ class Resolver(ABC):
         self._finalize()
         return None
 
-    def sync_to_db(self) -> None:
-        for k, v in self.counts.items():
-            self.db._put_impl(k, v)
-
-    def relinquish_resources(self, resources: dict) -> bool:
-        for k, v in resources.items():
-            self.counts[k] += resources[k]
-        self.sync_to_db()
-
-    def acquire_resources(self, resources: dict) -> bool:
-        for k, v in resources.items():
-            self.counts[k] -= resources[k]
-        self.sync_to_db()
-
-    def get(self, tier: str = ''):
+    async def get(self, tier: str = ''):
         """
         returns the available resources from the scheduler
         if None specified returns entire dict
         since we are making query async this can be too right?
         """
-        return self.counts[tier]
+        pass
 
     async def query(self):
         """
